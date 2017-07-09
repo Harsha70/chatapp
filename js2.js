1c24c3eebd3f3815bdb3b14302056870d37ca6e6
harsha <manyamharsha112@gmail.com>
2017-07-09 21:23:08 +0530

    adding to all files to git

--- js2.js
+++ js2.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 
+server = require('http').createServer(app)
+io = require('socket.io').listen(server)
+
 app.set('view engine','ejs');
 
 //opening home h2.html page using express
 app.use(express.static(__dirname+'/public_static'))
 
-app.set('port',process.env.PORT || 5000)
+//app.set('port',process.env.PORT || 5000)
+server.listen(process.env.PORT || 3000);
+console.log('Server Running...on 3000');
 
 var Datastore=require('nedb')
-var db=new Datastore({filename:'store2.db',autoload:true})
+var db=new Datastore({filename:'store.db',autoload:true})
 
 
 app.get('/',function(req,res){
@@ -52,11 +57,28 @@ app.get('/search',function(req,res){
 	})
 })
 
-
-
-app.listen(app.set('port'), function () {
-  console.log('Example app listening on port 5000!')
+app.get('/chat/:name',function(req,res){
+	var a=req.params.name;
+	db.find({'name':a},function(err,result){
+		res.render('chat',{result:result})
+	})
+	
 })
+
+//*************SOCKET.IO******************//
+
+io.sockets.on('connection',function(socket){
+	console.log('socket connected ...')
+
+	socket.on('send message',function(data){
+	console.log('message sent');
+	io.sockets.emit('new message',{msg:data})
+	});
+})
+
+/*app.listen(app.set('port'), function () {
+  console.log('Example app listening on port 5000!')
+})*/
 
 
 
